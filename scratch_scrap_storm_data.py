c771523b89d0d303e46a8edca8adc14e09fa4ce4
Aniket Jivani <ajivani@umich.edu>
2024-09-02 14:09:30 -0400

    ability to scrap and download data by year

--- scratch_scrap_storm_data.py
+++ scratch_scrap_storm_data.py
@@ -1,4 +1,5 @@
-# CCMC CME Scoreboard
+# %% CCMC CME Scoreboard
+YEAR_TO_STUDY = 2017
 # %%
 import requests
 from bs4 import BeautifulSoup
@@ -17,7 +18,7 @@ def fetch_webpage(url):
         return None
 # %%
 
-YEAR_TO_STUDY = 2014
+
 
 url_to_scrap = urljoin("https://kauai.ccmc.gsfc.nasa.gov/CMEscoreboard/PreviousPredictions/", f"{YEAR_TO_STUDY}/")
 
@@ -28,5 +29,34 @@ html_content = fetch_webpage(url_to_scrap)
 soup = BeautifulSoup(html_content, 'html.parser')
 cme_date_tags = soup.find_all('b', string=re.compile(r'CME: *'))
 
-
+arr_time_tags = soup.find_all('td', string=re.compile(r'(Actual Shock Arrival Time:.*|This CME was not .*)'))
 # %% Export content to file
+
+# Create two column dataframe
+
+# for arrival time, if tag is `This CME was not ...`, then arrival time is NaN
+# If tag is `Actual Shock Arrival Time: ...`, then arrival time is the date
+
+all_cme_dates = []
+
+for entry in cme_date_tags:
+    cme_info = entry.text.split('CME: ')[1].split('-CME')[0]
+    cme_date = datetime.strptime(cme_info, '%Y-%m-%dT%H:%M:%S')
+
+    all_cme_dates.append(cme_date)
+
+all_arrival_times = []
+for entry in arr_time_tags:
+    if 'This CME was not' in entry.text:
+        all_arrival_times.append(None)
+    else:
+        arr_time = entry.text.split('Actual Shock Arrival Time: ')[1]
+        all_arrival_times.append(datetime.strptime(arr_time, '%Y-%m-%dT%H:%MZ'))
+
+# dataframe (convert None to NaN)
+cme_records_df = pd.DataFrame({'CME Date': all_cme_dates, 'Arrival Time': all_arrival_times})
+
+cme_icme_df = cme_records_df[cme_records_df['Arrival Time'].notnull()]
+
+cme_icme_df.to_csv(f"data/processed_data/cme_icme_{YEAR_TO_STUDY}.csv", index=False)
+# %%
